077d56e353b7d6b8eb99dbe4ff09cfe4017bf662
vombat11 <gulyas.kata11@gmail.com>
2022-09-29 10:43:57 +0200

    js done

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,79 @@
+const year = [
+    {
+        "month": "January",
+        "id": "jan",
+        "nth": 1,
+        "days": 31,
+    },
+    {
+        "month": "February",
+        "id": "febr",
+        "nth": 2,
+        "days": 28,
+    },
+    {
+        "month": "March",
+        "id": "marc",
+        "nth": 3,
+        "days": 31,
+    },
+    {
+        "month": "April",
+        "id": "apr",
+        "nth": 4,
+        "days": 30,
+    },
+    {
+        "month": "May",
+        "id": "may",
+        "nth": 5,
+        "days": 31,
+    },
+    {
+        "month": "June",
+        "id": "june",
+        "nth": 6,
+        "days": 30,
+    },
+    {
+        "month": "July",
+        "id": "july",
+        "nth": 7,
+        "days": 31,
+    },
+    {
+        "month": "August",
+        "id": "aug",
+        "nth": 8,
+        "days": 31,
+    },
+    {
+        "month": "September",
+        "id": "sept",
+        "nth": 9,
+        "days": 30,
+    },
+    {
+        "month": "October",
+        "id": "oct",
+        "nth": 10,
+        "days": 31,
+    },
+    {
+        "month": "November",
+        "id": "nov",
+        "nth": 11,
+        "days": 30,
+    },
+    {
+        "month": "December",
+        "id": "dec",
+        "nth": 12,
+        "days": 31,
+    }
+];
+
+
 const monthComponent = function (nth, name, days) {
     let daysHtml = "";
 
@@ -22,16 +98,25 @@ const dayComponent = function (dayCount) {
 
 console.log("Hello")
 
+console.log(year[0].days)
+
 const rootElement = document.querySelector("#root")
 
 rootElement.insertAdjacentHTML("beforeend", "<button>Show Calendar</button>")
 const buttonElement = rootElement.querySelector("button")
+
+let monthIndex = 0;
+
 buttonElement.addEventListener("click", function() {
-//     rootElement.insertAdjacentHTML("beforeend", monthComponent(1, "january", 31))
-//     rootElement.insertAdjacentHTML("beforeend", monthComponent(1, "february", 28))
+    if (monthIndex < 12) {
+        rootElement.insertAdjacentHTML("beforeend", monthComponent(year[monthIndex].nth, year[monthIndex].month, year[monthIndex].days));
+        monthIndex++
+    } else {
+        buttonElement.setAttribute("disabled", "")
+    }
  })
 
-rootElement.insertAdjacentHTML("beforeend", monthComponent(1, "january", 31))
-rootElement.insertAdjacentHTML("beforeend", monthComponent(1, "february", 28))
+// rootElement.insertAdjacentHTML("beforeend", monthComponent(1, "january", 31))
+// rootElement.insertAdjacentHTML("beforeend", monthComponent(1, "february", 28))
 
 // rootElement.style.color = "blue"  STYLE CSAK CSS-BEN!!!
